fa260f343f80d305e3ea6321ec1e6902b11cc032
MateuszSzczukiewicz <szcmat6277@gmail.com>
2025-03-13 22:05:34 +0100

    added new tables

--- link_sharing_app/schema.sql
+++ link_sharing_app/schema.sql
@@ -1,7 +1,19 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS link;
 
 CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT,
+    image_url TEXT
 );
+
+CREATE TABLE link (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    platform TEXT NOT NULL CHECK ( platform in ('GitHub', 'Frontend_Mentor', 'Twitter', 'LinkedIn', 'YouTube', 'Facebook', 'Twitch', 'Dev.to', 'Codewars', 'Codepen', 'freeCodeCamp', 'GitLab', 'Hashnode', 'Stack_Overflow') ),
+    url TEXT UNIQUE NOT NULL
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+)
--- tests/data.sql
+++ tests/data.sql
@@ -1,10 +1,29 @@
-INSERT INTO user (email, password)
+INSERT INTO user (email, password, first_name, last_name, image_url)
 VALUES
 (
     'test@gmail.com',
-    'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'
+    'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f',
+    'Test',
+    'Testowy',
+    'https://link_to_image.com'
 ),
 (
     'other@wp.pl',
-    'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79'
+    'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79',
+    'Anonimowy',
+    'Anonim',
+    'https://link_to_image2.com'
 );
+
+INSERT INTO link (user_id, platform, url)
+VALUES
+(
+    '1',
+    'GitHub',
+    'https://github.com/TestTestowy'
+),
+(
+    '2',
+    'LinkedIn',
+    'https://www.linkedin.com/in/anonimowy-anonim'
+)
